6b35ecfd61d62bc62b010550415d9064d98e6fc9
Lucas <lucasgarciasouza12@gmail.com>
2025-02-07 14:03:17 -0300

    testando denovo

--- poo2/trabDev/demo/src/main/java/com/example/Main.java
+++ poo2/trabDev/demo/src/main/java/com/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Agora vai!");
+        System.out.println("vAI");
     }
 }
