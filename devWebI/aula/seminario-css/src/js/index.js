cd756878c632949a1fe0907a3e8d1a20e9cc8d99
Lucas <lucasgarciasouza12@gmail.com>
2025-03-31 14:53:58 -0300

    jkjk

--- devWebI/aula/seminario-css/src/js/index.js
+++ devWebI/aula/seminario-css/src/js/index.js
@@ -1,16 +1,24 @@
-const inputs = document.querySelectorAll("input[name='unit']");
+const inputs = document.querySelectorAll("input");
 const box = document.getElementById("box");
 
 inputs.forEach((input) => {
-  input.addEventListener("change", () => {
-    aplicarUnidade(input.value);
+  input.addEventListener("input", () => {
+    alterarUnidade(input.value);
   });
 });
 
-function aplicarUnidade(unidade) {
-  const tamanho = "10" + unidade;
-
-  box.style.width = tamanho;
-  box.style.height = tamanho;
-  box.textContent = `Unidade Atual: ${tamanho}`;
+function alterarUnidade(value) {
+  if (value == "vh") {
+    box.style.height = `100${value}`;
+    box.style.height = `Unidade Atual: ${box.style.height}`;
+  } else if (value == "ex") {
+    box.style.fontSize = `7${value}`;
+    box.textContent = `Unidade Atual: ${box.style.fontSize}`;
+    box.style.width = "95%";
+  } else {
+    box.style.width = "100" + value;
+    box.textContent = `Unidade Atual: ${box.style.width}`;
+    box.style.height = "100px";
+    box.style.fontSize = `16px`;
+  }
 }
