48039ec2f647507f818ad0ec426e3a6946aec4e2
Lucas <lucasgarciasouza12@gmail.com>
2025-05-07 13:59:46 -0300

    Implementação do padrão Strategy para cálculo de frete
    - Adicionada a classe CalculadoraDeFrete que utiliza a estratégia de cálculo de frete.
    - Implementadas as classes de estratégia: FreteEconomico, FreteExpresso e FreteInternacional.
    - Criada a interface CalculaFrete para definir o contrato das estratégias de cálculo.
    - Implementado o método main para demonstrar o uso das diferentes estratégias de frete.
    - Configuração do projeto Maven com pom.xml e nbactions.xml para execução e depuração.

---
+++ projeto-sistemas/padroes-projeto/abstract factory/ExemploAbstractFactory/src/main/java/classes/WebFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import interfaces.Notificacao;
+import interfaces.PlataformaFactory;
+import interfaces.Toast;
+
+/**
+ *
+ * @author lucas
+ */
+public class WebFactory implements PlataformaFactory{
+
+    @Override
+    public Notificacao criarNotificacao() {
+        return new WebNotificacao();
+    }
+
+    @Override
+    public Toast criarToast() {
+         return new WebToast();
+    }
+    
+}
