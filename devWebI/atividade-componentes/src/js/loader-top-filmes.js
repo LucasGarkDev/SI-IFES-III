ad3700b8fa72b6b60c4b85e6e3005610ad71b8a0
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-06-14 11:19:56 -0300

    updated render

--- devWebI/atividade-componentes/src/js/loader-top-filmes.js
+++ devWebI/atividade-componentes/src/js/loader-top-filmes.js
@@ -1,4 +1,4 @@
-import { renderIMG,renderA,renderButton } from "./render.js";
+import { renderIMG, renderA, renderButton } from "./render.js";
 (() => {
   const url = "./src/data/topMovies.json";
   const options = {
@@ -33,34 +33,52 @@ import { renderIMG,renderA,renderButton } from "./render.js";
 
 function render(data) {
   const autoLoadTopFilmes = document.getElementById("autoLoadTopFilmes");
+  const autoLoadTopFilmesBtns = document.getElementById(
+    "autoLoadTopFilmesBtns"
+  );
 
-  // <div class="carousel-item active">
-  //   <img src="./src/img/exemplo.png" class="d-block w-100" alt="..." />
-  // </div>;
-  for (let i = 0; i < data.length; i++) {
-    var carouselitem = document.createElement("div");
-		var img = document.createElement("img");
-
-    newScriptLib.setAttribute("src", srcsLib[i]);
-    autoscripts.appendChild(newScriptLib);
-
-    console.log(`%c [SISTEMA]: Nova Lib: ${srcsLib[i]}`, "color: #ffa500");
-  }
-
-	// <button
+  // <button
   //         type="button"
   //         data-bs-target="#carouselExampleIndicators"
   //         data-bs-slide-to="2"
   //         aria-label="Slide 3"
   //       ></button>
-  for (let i = 0; i < srcs.length; i++) {
-    const src = srcs[i];
-    const link = fonte + src + ".js";
-    var newScript = document.createElement("script");
+  for (let i = 0; i < data.length; i++) {
+    var button = document.createElement("button");
 
-    newScript.setAttribute("src", link);
-    autoscripts.appendChild(newScript);
+    // configurações do botão
+    button.setAttribute("type", "button");
+    button.setAttribute("data-bs-target", "#carouselExampleIndicators");
+    button.setAttribute("data-bs-slide-to", i);
+    button.setAttribute("aria-label", `Slide ${i}`);
 
-    console.log(`%c [SISTEMA]: Carregando script: ${link}`, "color: #ff00ff");
+    // adiciona o button na lista
+    autoLoadTopFilmesBtns.appendChild(button);
+
+    console.log(`%c [SISTEMA]: Carregado Botão: ${i}`, "color: #00ff00");
+  }
+
+  // <div class="carousel-item active">
+    // <img src="./src/img/exemplo.png" class="d-block w-100" alt="..." />
+  // </div>;
+  for (let j = 0; j < data.length; j++) {
+    const topFilme = data[j];
+    var carrouselItem = document.createElement("div");
+
+    if (j == 0) {
+      carrouselItem.setAttribute("class", "carousel-item active");
+    } else {
+      carrouselItem.setAttribute("class", "carousel-item");
+    }
+
+    renderIMG(carrouselItem,"d-block w-100", topFilme.image, topFilme.fullTitle);
+
+    // adiciona o div carrossel item na lista
+    autoLoadTopFilmes.appendChild(carrouselItem);
+
+    console.log(
+      `%c [SISTEMA]: Carregado Carrossel image: ${topFilme.fullTitle}`,
+      "color: #00ff00"
+    );
   }
 }
