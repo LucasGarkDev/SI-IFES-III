cab624bbd991394a94dbba79fc5fda0dbb73b837
Lucas <lucasgarciasouza12@gmail.com>
2025-06-16 13:32:06 -0300

    montando o basico

---
+++ projeto-sistemas/padroes-projeto/template method/src/Pagamento.ts
@@ -0,0 +1,18 @@
+export abstract class Pagamento {
+  constructor(protected valorBase: number) {}
+
+  public processar(): void {
+    const desconto = this.calcularDesconto();
+    const total = this.valorBase - desconto;
+    this.exibirResumo(total);
+  }
+
+  protected abstract calcularDesconto(): number;
+
+  protected exibirResumo(total: number): void {
+    const div = document.getElementById("resumo");
+    if (div) {
+      div.innerHTML = `Valor com desconto: <strong>R$ ${total.toFixed(2)}</strong>`;
+    }
+  }
+}
