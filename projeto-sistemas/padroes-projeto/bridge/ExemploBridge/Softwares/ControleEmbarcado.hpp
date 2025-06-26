827cb99f6dcf8e7edfe680ce57556b24ffbdb4e6
Lucas <lucasgarciasouza12@gmail.com>
2025-06-26 06:41:07 -0300

    Adiciona classes Microcontrolador e ControleEmbarcado com implementação inicial

--- projeto-sistemas/padroes-projeto/bridge/ExemploBridge/Softwares/ControleEmbarcado.hpp
+++ projeto-sistemas/padroes-projeto/bridge/ExemploBridge/Softwares/ControleEmbarcado.hpp
@@ -1,13 +1,13 @@
-// Softwares/EditorTexto.hpp
+// Softwares/ControleEmbarcado.hpp
 #ifndef CONTROLEEMBARCADO_HPP
 #define CONTROLEEMBARCADO_HPP
 
 #include "../Software.hpp"
 #include <iostream>
 
-class controleEmbarcado : public Software {
+class ControleEmbarcado : public Software {
 public:
-    controleEmbarcado(HardwareArchitecture* arquitetura) : Software(arquitetura) {}
+    ControleEmbarcado(HardwareArchitecture* arquitetura) : Software(arquitetura) {}
 
     void rodar() override {
         std::cout << "Iniciando Firmware..." << std::endl;
