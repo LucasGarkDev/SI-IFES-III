ad5f67ea297b4181befb716423ff622a36e26bb4
Lucas <lucasgarciasouza12@gmail.com>
2025-08-05 17:19:26 -0300

    Initialize MeuClienteUDP project with NetBeans configuration files and a basic UDP client structure
    - Added project properties and configuration files for NetBeans IDE.
    - Created main class MeuClienteUDP with a placeholder main method.
    - Set Java version to 17 and configured build directories.

---
+++ SISTDIS/MeuClienteUDP/src/meuclienteudp/MeuClienteUDP.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meuclienteudp;
+
+/**
+ *
+ * @author lucas
+ */
+public class MeuClienteUDP {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
