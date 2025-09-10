b0fbdd727beb435a57e5c65d6e0f7bb92582cc9b
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-09-09 22:48:13 -0300

    FIXED BUGS VITE_ENV

--- devWebII/locadora/frontend/default/src/api/api.js
+++ devWebII/locadora/frontend/default/src/api/api.js
@@ -1,9 +1,8 @@
-import "dotenv/config";
 import axios from "axios";
 
-const url = import.meta.env.VITE_ENV; // não process.env
+const {VITE_ENV} = import.meta.env; // não process.env
 // const { NODE_ENV } = process.env;
-// const url = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const url = VITE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 const resArtists = await axios.get(`${url}/artists`);
 const resSongs = await axios.get(`${url}/songs`);
 export const artistArray = resArtists.data;
