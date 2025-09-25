346e63f82047ca1ca5946f7176440016cef9836a
Lucas <lucasgarciasouza12@gmail.com>
2025-09-25 13:54:10 -0300

    vfg

---
+++ SISTDIS/projeto-ringo/src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+import agente.Agente;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo: agente do usuário 0
+        Agente agente = new Agente(2);
+        agente.iniciar();
+    }
+}
