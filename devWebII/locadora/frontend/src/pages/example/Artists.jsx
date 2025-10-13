2d297a5c9088a0a7f1808e6bf104eb21ccd4d40f
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-10-13 19:12:04 -0300

    Delete devWebII/locadora/frontend/src directory

--- devWebII/locadora/frontend/src/pages/example/Artists.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import Main from "../../components/Home"
-
-const Artists = () => {
-  return <Main type="artists"/>;
-};
-
-export default Artists;
