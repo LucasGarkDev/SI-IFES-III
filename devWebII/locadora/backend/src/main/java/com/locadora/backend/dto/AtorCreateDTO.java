43e306f5316833219db192e9b24e4b9f4d5480ab
Lucas <lucasgarciasouza12@gmail.com>
2025-10-15 15:27:29 -0300

    dgdf

--- devWebII/locadora/backend/src/main/java/com/locadora/backend/dto/AtorCreateDTO.java
+++ devWebII/locadora/backend/src/main/java/com/locadora/backend/dto/AtorCreateDTO.java
@@ -7,16 +7,10 @@ import java.time.LocalDate;
 public class AtorCreateDTO {
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
-//    private String nacionalidade;
-//    @Past(message = "Data de nascimento deve estar no passado")
-//    private LocalDate dataNascimento;
 
     // getters/setters
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-//    public String getNacionalidade() { return nacionalidade; }
-//    public void setNacionalidade(String nacionalidade) { this.nacionalidade = nacionalidade; }
-//    public LocalDate getDataNascimento() { return dataNascimento; }
-//    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+
 }
 
