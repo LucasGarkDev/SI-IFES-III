dc544fae7a17146f58c39908ccad5c946e099592
Lucas <lucasgarciasouza12@gmail.com>
2025-10-28 13:01:02 -0300

    de

--- dispositivosmoveis/mercadim/lib/main.dart
+++ dispositivosmoveis/mercadim/lib/main.dart
@@ -1,22 +1,21 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'firebase_options.dart'; // gerado automaticamente pelo flutterfire configure
+import 'firebase_options.dart';
 import 'presentation/pages/feed_page.dart';
 import 'presentation/pages/signup_page.dart';
+import 'presentation/pages/login_page.dart';        // ✅
 import 'presentation/pages/favoritos_page.dart';
 import 'presentation/pages/filtro_page.dart';
 import 'presentation/pages/buscar_anuncios_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Inicializa o Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const ProviderScope(child: MercadimApp()));
 }
 
@@ -28,12 +27,10 @@ class MercadimApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mercadim',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green,
-      ),
-      initialRoute: '/signup',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green),
+      initialRoute: '/login',                       // ✅ começa no login
       routes: {
+        '/login': (_) => const LoginPage(),        // ✅ nova rota
         '/signup': (_) => const SignUpPage(),
         '/feed': (_) => const FeedPage(),
         '/favoritos': (_) => const FavoritosPage(usuarioId: 'u_atual'),
