3db13ed91a0e54c27de5b833744268446b453274
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-10-29 15:42:19 -0300

    updated

---
+++ devWebII/locadora/frontend/src/assets/database/titulos.js
@@ -0,0 +1,23 @@
+import { dataStore, carregarBanco } from "../../service/api.js";
+
+
+// Sincronizar apenas "diretores"
+await carregarBanco("diretores");
+console.log("[DB CONTROLLER DIRETOR]: ", dataStore);
+let diretoresArray = dataStore.diretoresArray; // cria cópia local mutável
+
+if (!diretoresArray || diretoresArray.length === 0) {
+	console.log("Usando dados locais para diretores.js");
+	diretoresArray = [
+		{
+			_id: 1,
+			name: "James Cameron",
+		},
+		{
+			_id: 2,
+			name: "William Wisher",
+		},
+	];
+}
+
+export { diretoresArray };
