1ac8d9ff5acf46f512b8f4673ba24f38d42592c5
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-11-01 16:20:12 -0300

    UPDATED

--- devWebII/locadora/backend/start.cmd
+++ devWebII/locadora/backend/start.cmd
@@ -4,3 +4,7 @@ git pull
 
 set "id=%random%"
  gradlew clean bootRun --no-daemon --info >> %random%-WINDOWS-SERVER-%COMPUTERNAME%.txt 2>&1
+ git add *.txt
+git commit -m "WINDOWS SERVER CRASH LOGS"
+git pull
+git push origin main
