fa42443c84f71c5614a96e28a4273a3a21266d0a
Lucas <lucasgarciasouza12@gmail.com>
2025-11-06 14:26:33 -0300

    concertando a falta de encontro com o item

--- devWebII/locadora/frontend2/src/services/locacaoService.js
+++ devWebII/locadora/frontend2/src/services/locacaoService.js
@@ -1,13 +1,7 @@
-// src/services/locacaoService.js
-import axios from "axios";
+import api from "./api";
 
-const API_URL = "http://localhost:8085/api/locacoes"; // ✅ coloque o /api aqui
-
-export const getLocacoes = () => axios.get(API_URL);
-export const createLocacao = (data) => axios.post(API_URL, data);
-export const devolverLocacao = (data) =>
-  axios.post(`${API_URL}/devolucao`, data);
-export const updateLocacao = (id, data) =>
-  axios.put(`${API_URL}/${id}`, data);
-export const deleteLocacao = (id) =>
-  axios.delete(`${API_URL}/${id}`);
+export const getLocacoes = () => api.get("/api/locacoes");
+export const createLocacao = (data) => api.post("/api/locacoes", data);
+export const devolverLocacao = (data) => api.post("/api/locacoes/devolucao", data);
+export const cancelarLocacao = (id) => api.delete(`/api/locacoes/${id}`);
+export const updateLocacao = (id, data) => api.put(`/api/locacoes/${id}`, data);
