821d10950152c0b7f68140b1f46fa0cd6dfa15ec
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-11-12 16:13:41 -0300

    atualiza dependências e refatora componentes de formulário

---
+++ devWebII/locadora/frontend/src/assets/database/clientes.js
@@ -0,0 +1,25 @@
+import { carregarBanco, dataStore } from "../../service/api.js";
+
+// Sincronizar apenas o banco necessario
+await carregarBanco("clientes");
+console.log("[DB CONTROLLER CLIENTES]: ", dataStore);
+let atoresArray = dataStore.atoresArray; // cria cópia local mutável
+if (!atoresArray || atoresArray.length === 0) {
+  console.log("Usando dados locais para atores.js");
+  atoresArray = [
+    {
+      _id: 1,
+      nome: "Linda Hamilton",
+    },
+    {
+      _id: 2,
+      nome: "Arnold Schwarzenegger",
+    },
+    {
+      _id: 3,
+      nome: "Robert Patrick",
+    },
+  ];
+}
+
+export { atoresArray };
